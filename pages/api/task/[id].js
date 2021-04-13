b223af10c568c57cc7e1e2d3c4c235714ab07edf
David Castillo <hola@davecastworks.com>
2021-04-13 00:47:15 -0400

    [add] route for dynamic id task with POST, PUT methods

---
+++ pages/api/task/[id].js
@@ -0,0 +1,52 @@
+import { ObjectId } from "mongodb";
+
+import { connectToDatabase } from "../../../middleware/database";
+
+const getOneTask = async (request, response, id) => {
+  const { db } = await connectToDatabase();
+  const task = await db.collection("tasks").findOne({ _id: id });
+
+  response.statusCode = 200; //ok
+  response.setHeader("Content-type", "application/json");
+  response.end(JSON.stringify({ ...task }));
+};
+
+const deleteOneTask = async (request, response, id) => {
+  const { db } = await connectToDatabase();
+  const deleteTask = await db.collection("tasks").deleteOne({ _id: id });
+
+  response.statusCode = 200; //ok
+  response.setHeader("Content-type", "application/json");
+  response.end(JSON.stringify({ deletedCount: deleteTask.deletedCount }));
+};
+
+const updateOneTask = async (request, response, id) => {
+  const { db } = await connectToDatabase();
+  const task = await db
+    .collection("tasks")
+    .updateOne({ _id: id }, { $set: request.body });
+
+  response.statusCode = 200; //ok
+  response.setHeader("Content-type", "application/json");
+  response.end(JSON.stringify({ modifiedCount: task.modifiedCount }));
+};
+
+export default async (request, response) => {
+  const id = new ObjectId(`${request.query.id}`);
+
+  switch (request.method) {
+    case "GET":
+      await getOneTask(request, response, id);
+      break;
+    case "PUT":
+      await updateOneTask(request, response, id);
+      break;
+    case "DELETE":
+      await deleteOneTask(request, response, id);
+      break;
+    default:
+      response.statusCode = 404;
+      response.setHeader("Content-type", "application/json");
+      response.end(JSON.stringify({ message: "Metodo no es permitido" }));
+  }
+};
