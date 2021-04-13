d957f3b970457ebeddb2d435394eeea07a9dea15
David Castillo <hola@davecastworks.com>
2021-04-12 23:52:46 -0400

    [add] component TodoList

---
+++ components/TodoList/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+// import Task from '@components/Task'
+
+// import { getTasks } from '@services/tasks'
+
+const TodoList = ({ tasks, setTasks, loadingTasks, setLoadingTasks }) => {
+  // useEffect(() => {
+  //   setLoadingTasks(true)
+  //   getTasks().then((res) => {
+  //     setTasks([...res.data.tasks] as any)
+  //     setLoadingTasks(false)
+  //   })
+  // }, [])
+
+  // if (loadingTasks) {
+  //   return <div>Loading...</div>
+  // }
+
+  return (
+    <section className="TaskList">
+      Todo List
+      {/* {tasks.length > 0 &&
+        tasks
+          .map(({ _id, description, status }: TTask) => {
+            return (
+              <Task
+                key={_id}
+                information={{ _id, description, status }}
+                tasks={tasks}
+                setTasks={setTasks}
+              />
+            )
+          })
+          .reverse()} */}
+    </section>
+  );
+};
+
+export default TodoList;
