4bdf4696e018fa90d7accd3789a440a8f672d907
David Castillo <hola@davecastworks.com>
2021-04-12 23:53:57 -0400

    [add] ultils folder

---
+++ utils/requests.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export default function request(
+  url,
+  config = {
+    method: "GET",
+    params: {},
+    body: {},
+    headers: {
+      "Accept-Language": "en-US,en;q=0.5",
+    },
+    mode: "",
+    cache: "",
+  }
+) {
+  return axios.request({
+    ...config,
+    data: config.body,
+    url,
+  });
+}
